0b03c2adda36b20868377b75132787f77fc5fcaf
EvertonTenorio <everton.tenorio10@gmail.com>
2026-03-20 23:04:41 -0300

    Removendo arquivos de teste para subir no ambiente.

--- src/app/teste/page.tsx
+++
@@ -1,8 +0,0 @@
-import "@/assets/styles/teste/index.css"
-import PostsFeature from "@/features/testes/posts";
-
-export default function TestPage() {
-    return (
-        <PostsFeature />
-    );
-}
